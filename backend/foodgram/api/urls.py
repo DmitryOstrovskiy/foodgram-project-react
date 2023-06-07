e0511f439dbbaa430c29ba852f31e93f6e4f49f2
Дмитрий Островский <ostrov_dm85@mail.ru>
2023-06-07 16:56:23 +0300

    Исправления в urls api№2

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,7 +2,7 @@ from api.views import (CartViewSet, CreateUserView, DownloadCart,
                        FavoriteViewSet, IngredientViewSet, RecipeViewSet,
                        SubscribeViewSet, TagViewSet)
 from django.urls import include, path
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
