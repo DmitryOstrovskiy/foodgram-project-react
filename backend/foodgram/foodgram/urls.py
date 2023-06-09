d5c8bca21152f2dc41c3befc3898adef9d706821
Дмитрий Островский <ostrov_dm85@mail.ru>
2023-06-09 11:10:28 +0300

    Добавление в urls redoc

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -17,10 +17,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 if settings.DEBUG:
     import debug_toolbar
